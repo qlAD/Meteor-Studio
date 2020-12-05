66622f9f236a7f515d0755117ad666f90aff2094
qlad <3394543809@qq.com>
2020-12-05 22:37:14 +0800

    简单打印输出

---
+++ first/first.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+* File Name: first.cpp
+* Author: meteor
+* mail: 3394543809@qq.com
+* Created Time: Sat 14 Nov 2020 09:27:37 PM CST
+ ************************************************************************/
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "欢迎学习 C++ !" ; 
+    cout << endl;
+    cout << "请按任意建继续：" ;
+    cin.get();
+    return 0;
+}
